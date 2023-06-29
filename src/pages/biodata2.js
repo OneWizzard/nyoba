a16bf760e9dd2f2e559ac2f404e9436629564261
OneWizzard <kurniawanrachmadhani@gmail.com>
2023-06-29 22:32:10 +0700

    nyobain

---
+++ src/pages/biodata2.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const biodata2 = () => {
+  return (
+    <div>biodata iwan</div>
+  )
+}
+
+export default biodata2
---
+++ src/pages/contact.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const contact = () => {
+  return (
+    <div>berikut adalah kontak kami</div>
+  )
+}
+
+export default contact
